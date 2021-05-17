d88b6f65f367bab47dfe703a7bb8ea52f2b67747
iryna-vys <vys.iryna@gmail.com>
2021-05-17 15:23:48 +0300

    Updates

---
+++ precache-manifest.e8b2a4c1b79191b8d4f224ea3ac3ed5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8b9e28a9ee2fdea453279efa7f81ea",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e8f7346be9b797f34bf9",
+    "url": "./static/css/main.abb13962.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8f7346be9b797f34bf9",
+    "url": "./static/js/main.d5b200e1.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
